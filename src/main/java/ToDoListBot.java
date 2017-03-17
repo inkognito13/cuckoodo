92b21c10299b02efe53b85e643ba480cb26995e9
dtarasov <dtarasov@griddynamics.com>
2017-03-17 21:08:26 +0300

    init

---
+++ src/main/java/ToDoListBot.java
@@ -0,0 +1,32 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * @author Dmitry Tarasov
+ *         Date: 03/17/2017
+ *         Time: 20:03
+ */
+public class ToDoListBot extends TelegramLongPollingBot {
+    public String getBotToken() {
+        return "";
+    }
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                sendMessage(message); 
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "ToDoListBot";
+    }
+}
