c5d74948347bb9e19b944adee1d9d7f1be2289db
dtarasov <dtarasov@griddynamics.com>
2017-03-22 15:12:55 +0300

    Sql db on H2

--- src/main/java/hackaton/cuckoodobot/DataSource.java
+++ src/main/java/hackaton/cuckoodobot/DataSource.java
@@ -1,5 +1,12 @@
 package hackaton.cuckoodobot;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.*;
 
 /**
@@ -8,101 +15,179 @@ import java.util.*;
  *         Time: 08:54
  */
 public class DataSource {
-    //<groupId:<issueId:issue>>
-    Map<Long, Map<Long, Issue>> storage = new HashMap<Long, Map<Long, Issue>>();
+    private static final String GET_ISSUE = "select * from ISSUES LEFT JOIN REMINDERS on (ISSUES.ID = REMINDERS.ISSUE_ID) where ISSUES.ID=? and ISSUES.GROUP_ID=? ORDER BY ISSUES.ID";
+    private static final String GET_ISSUES_FOR_GROUP = "select * from ISSUES LEFT JOIN REMINDERS on (ISSUES.ID = REMINDERS.ISSUE_ID) where ISSUES.GROUP_ID=? ORDER BY ISSUES.ID";
+    private static final String GET_ISSUES_FOR_USER = "select * from ISSUES LEFT JOIN REMINDERS on (ISSUES.ID = REMINDERS.ISSUE_ID) where ISSUES.ASSIGNEE=? and ISSUES.GROUP_ID=? ORDER BY ISSUES.ID";
+    private static final String ADD_ISSUE = "insert into ISSUES(ID,GROUP_ID,TEXT,ASSIGNEE,DONE) VALUES (?,?,?,?,?)";
+    private static final String ADD_REMINDER = "insert into REMINDERS(ISSUE_ID,CREATED,TARGET) VALUES (?,?,?)";
+    private static final String DONE_ISSUE = "update ISSUES set DONE=true where ID=?";
+    private static final String DELETE_ISSUE = "delete from ISSUES where ID=?";
+    private static final String DELETE_REMINDER = "delete from REMINDERS where ISSUE_ID=?";
+    private static final String CHANGE_ASSIGNEE = "update ISSUES set ASSIGNEE=? where ID=?";
+    
+    private JdbcTemplate jdbcTemplate;
+
+    public DataSource(javax.sql.DataSource ds) {
+        this.jdbcTemplate = new JdbcTemplate(ds);
+    }
 
     public Issue getIssue(Long id, long groupId) {
-        if (storage.containsKey(groupId)) {
-            Map<Long, Issue> groupMessages = storage.get(groupId);
-            if (groupMessages.containsKey(id)) {
-                return groupMessages.get(id);
-            }
+        try {
+            return jdbcTemplate.queryForObject(
+                    GET_ISSUE,
+                    new Object[]{id, groupId},
+                    new IssueRowMapper()
+            );
+        } catch (DataAccessException e) {
+            System.err.println("Error getIssue id=" + id + " groupId=" + groupId);
+            e.printStackTrace();
         }
-
         return null;
     }
 
     List<Issue> getIssueForGroup(long groupId) {
-        Map<Long, Issue> groupMessages = storage.get(groupId);
-        if (groupMessages == null) {
-            groupMessages = new TreeMap<Long, Issue>();
-            storage.put(groupId, groupMessages);
+        try {
+            return jdbcTemplate.query(
+                    GET_ISSUES_FOR_GROUP,
+                    new Object[]{groupId},
+                    new IssueRowMapper()
+            );
+        } catch (DataAccessException e) {
+            System.err.println("Error getIssueForGroup groupId=" + groupId);
+            e.printStackTrace();
         }
-        return new ArrayList<Issue>(groupMessages.values());
+        return new ArrayList<>();
     }
 
-    Issue addIssue(Issue issue) {
-        Map<Long, Issue> groupMessages = storage.get(issue.getGroupId());
-        if (groupMessages == null) {
-            groupMessages = new TreeMap<Long, Issue>();
-            storage.put(issue.getGroupId(), groupMessages);
+    void addIssue(Issue issue) {
+        try {
+            issue.setId(System.currentTimeMillis());
+            jdbcTemplate.update(
+                    ADD_ISSUE,
+                    issue.getId(),
+                    issue.getGroupId(),
+                    issue.getText(),
+                    issue.getAssignee(),
+                    issue.getDone()
+            );
+
+            if (issue.getReminder() != null) {
+                jdbcTemplate.update(
+                        ADD_REMINDER,
+                        issue.getId(),
+                        new Timestamp(issue.getReminder().getCreated().getTimeInMillis()),
+                        new Timestamp(issue.getReminder().getTarget().getTimeInMillis())
+                );
+            }
+        } catch (DataAccessException e) {
+            System.err.println("Error addIssue issue=" + issue);
+            e.printStackTrace();
         }
-        groupMessages.put(issue.getId(), issue);
-        return issue;
     }
 
     boolean doneIssue(int idx, long groupId) {
-        if (storage.containsKey(groupId)) {
-            Map<Long, Issue> groupMessages = storage.get(groupId);
-
-            for (Long key : groupMessages.keySet()) {
-                idx--;
-                if (idx == 0) {
-                    groupMessages.get(key).setDone(true);
+        try {
+            List<Issue> issues = getIssueForGroup(groupId);
+            for (int i = 0; i < issues.size(); i++) {
+                if ((i + 1) == idx) {
+                    updateIssueToDone(issues.get(i).getId());
                     return true;
                 }
             }
+        } catch (SQLException e) {
+            System.err.println("Error done issue idx=" + idx + " groupId=" + groupId);
+            e.printStackTrace();
         }
-
         return false;
     }
 
-    Issue deleteIssue(int idx, long groupId) {
-        if (storage.containsKey(groupId)) {
-            Map<Long, Issue> groupMessages = storage.get(groupId);
+    private void updateIssueToDone(long issueId) throws SQLException {
+        jdbcTemplate.update(DONE_ISSUE, issueId);
+    }
 
-            if (groupMessages.size() >= idx) {
-                for (Long key : groupMessages.keySet()) {
-                    idx--;
-                    if (idx == 0) {
-                        return groupMessages.remove(key);
-                    }
+    Issue deleteIssue(int idx, long groupId) {
+        try {
+            List<Issue> issues = getIssueForGroup(groupId);
+            for (int i = 0; i < issues.size(); i++) {
+                if ((i + 1) == idx) {
+                    deleteIssueById(issues.get(i).getId());
+                    deleteReminderByIssueId(issues.get(i).getId());
+                    return issues.get(i);
                 }
             }
+        } catch (SQLException e) {
+            System.err.println("Error delete issue idx=" + idx + " groupId=" + groupId);
+            e.printStackTrace();
         }
         return null;
     }
 
-    boolean assigneeIssue(int idx, String newUser, long groupId) {
-        if (storage.containsKey(groupId)) {
-            Map<Long, Issue> groupMessages = storage.get(groupId);
+    private void deleteIssueById(long issueId) throws SQLException {
+        jdbcTemplate.update(DELETE_ISSUE, issueId);
+    }
 
-            if (groupMessages.size() >= idx) {
-                for (Long key : groupMessages.keySet()) {
-                    idx--;
-                    if (idx == 0) {
-                        groupMessages.get(key).setAssignee(newUser);
-                        return true;
-                    }
+    private void deleteReminderByIssueId(long issueId) throws SQLException {
+        jdbcTemplate.update(DELETE_REMINDER, issueId);
+    }
+
+    boolean assigneeIssue(int idx, String newUser, long groupId) {
+        try {
+            List<Issue> issues = getIssueForGroup(groupId);
+            for (int i = 0; i < issues.size(); i++) {
+                if ((i + 1) == idx) {
+                    updateIssueAssignee(issues.get(i).getId(), newUser);
+                    return true;
                 }
             }
+        } catch (SQLException e) {
+            System.err.println("Error assign issue idx=" + idx + " groupId=" + groupId + " newUser=" + newUser);
+            e.printStackTrace();
         }
         return false;
     }
 
-    ArrayList<Issue> getAllIssueForUser(String username, long groupId) {
-        ArrayList<Issue> res = new ArrayList<Issue>();
-        if (storage.containsKey(groupId)) {
-            Map<Long, Issue> groupMessages = storage.get(groupId);
-
-            for (Issue issue : groupMessages.values()) {
-                if (issue.getAssignee().toLowerCase().equals(username.toLowerCase())) {
-                    res.add(issue);
-                }
-            }
-        }
-
-        return res;
+    private void updateIssueAssignee(long issueId, String newUser) throws SQLException {
+        jdbcTemplate.update(CHANGE_ASSIGNEE, newUser, issueId);
     }
 
+    List<Issue> getAllIssueForUser(String username, long groupId) {
+        List<Issue> result = new ArrayList<>();
+        try {
+            return jdbcTemplate.query(
+                    GET_ISSUES_FOR_USER,
+                    new Object[]{username, groupId},
+                    new IssueRowMapper()
+            );
+        } catch (DataAccessException e) {
+            System.err.println("Error getAllIssueForUser username=" + username + " groupId=" + groupId);
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private Issue issueFromRS(ResultSet rs) throws SQLException {
+        Issue issue = new Issue(
+                rs.getLong("ISSUES.GROUP_ID"),
+                rs.getString("ISSUES.TEXT"),
+                rs.getString("ISSUES.ASSIGNEE")
+        );
+        issue.setId(rs.getLong("ISSUES.ID"));
+        issue.setDone(rs.getBoolean("ISSUES.DONE"));
+        if (rs.getLong("REMINDERS.ISSUE_ID") != 0) {
+            Calendar target = Calendar.getInstance();
+            target.setTimeInMillis(rs.getTimestamp("REMINDERS.TARGET").getTime());
+            Calendar created = Calendar.getInstance();
+            created.setTimeInMillis(rs.getTimestamp("REMINDERS.CREATED").getTime());
+            Reminder reminder = new Reminder(created, target);
+            issue.setReminder(reminder);
+        }
+        return issue;
+    }
+
+    private class IssueRowMapper implements RowMapper<Issue> {
+        @Override
+        public Issue mapRow(ResultSet resultSet, int i) throws SQLException {
+            return issueFromRS(resultSet);
+        }
+    }
 }
--- src/main/java/hackaton/cuckoodobot/Main.java
+++ src/main/java/hackaton/cuckoodobot/Main.java
@@ -1,10 +1,17 @@
 package hackaton.cuckoodobot;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
 import org.quartz.Scheduler;
 import org.quartz.impl.StdSchedulerFactory;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import javax.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  * @author Dmitry Tarasov
@@ -14,6 +21,10 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 public class Main {
 
     private final static String BOT_TOKEN = "BOT_TOKEN";
+    private final static String DB_URL = "jdbc:h2:~/cuckoodo;CIPHER=AES"; //db file will be created in home dir with AES encryption
+    private final static String DB_USER = "DB_USER";
+    private final static String DB_PASSWORD = "DB_PASSWORD";
+    private final static String DB_FILE_PASSWORD = "DB_FILE_PASSWORD";
 
     public static void main(String[] args) throws Exception {
 
@@ -23,10 +34,29 @@ public class Main {
         ApiContextInitializer.init();
 
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        
+        String dbUser = System.getenv(DB_USER);
+        String dbPassword = System.getenv(DB_FILE_PASSWORD)+" "+System.getenv(DB_PASSWORD); //format is <file password><space><user password>        
+        
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(DB_URL, dbUser, dbPassword);
+        flyway.migrate();
+        
         try {
-            telegramBotsApi.registerBot(new CuckoodoBot(System.getenv(BOT_TOKEN), scheduler));
+            telegramBotsApi.registerBot(new CuckoodoBot(System.getenv(BOT_TOKEN), scheduler,new DataSource(initDataSource(dbUser,dbPassword))));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+    
+    private static javax.sql.DataSource initDataSource(String user,String pass){
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.h2.Driver");
+        config.setJdbcUrl(DB_URL);
+        config.setUsername(user);
+        config.setPassword(pass);
+        config.setMaximumPoolSize(5);
+        config.setAutoCommit(true);
+        return new HikariDataSource(config);
     }
 }
